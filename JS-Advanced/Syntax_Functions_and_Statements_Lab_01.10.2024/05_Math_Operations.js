f1bb7179e2f8f5137a1c71879da05ddc1f97f616
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-10 11:27:07 +0200

    Create 05_Math_Operations.js

---
+++ JS-Advanced/Syntax_Functions_and_Statements_Lab_01.10.2024/05_Math_Operations.js
@@ -0,0 +1,31 @@
+function mathOps(num1, num2, operator) {
+
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+    }
+
+    console.log(result);
+    
+}
+
+mathOps(5, 6, '+');
+mathOps(3, 5.5, '*');
