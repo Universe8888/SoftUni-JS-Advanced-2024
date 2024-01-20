249177c9b9ef229a5b830b4d21629bbe2aab5de6
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-20 12:26:33 +0200

    Update 01_Calorie_Object.js

--- JS-Advanced/Objects_and_Composition_Exercise_01.18.2024/01_Calorie_Object.js
+++ JS-Advanced/Objects_and_Composition_Exercise_01.18.2024/01_Calorie_Object.js
@@ -24,5 +24,3 @@ function calorieObject(input) {
 console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
 console.log('-----------------');
 console.log(calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']));
-
-//100/100
