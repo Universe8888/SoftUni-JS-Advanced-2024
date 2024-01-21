d8a1bf3f633989c29b73140b886ffe4ae0157ed6
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 12:43:26 +0200

    Update subtract.js

--- JS-Advanced/DOM_Introduction_Exercise_01.22.2024/01. Subtraction/subtract.js
+++ JS-Advanced/DOM_Introduction_Exercise_01.22.2024/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumber = Number(document.getElementById('firstNumber').value);
+    let secondNumber = Number(document.getElementById('secondNumber').value);
+
+    let result = document.getElementById('result');
+    result.textContent = firstNumber - secondNumber;
 }
