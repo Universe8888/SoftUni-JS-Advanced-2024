c537cae935117b1ceb06af95c9278315ebb32c48
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 12:55:14 +0200

    Update search.js

--- JS-Advanced/DOM_Introduction_Exercise_01.22.2024/04. Search in List/search.js
+++ JS-Advanced/DOM_Introduction_Exercise_01.22.2024/04. Search in List/search.js
@@ -1,3 +1,25 @@
+/**
+ * search.js - Search in List
+ * 
+ * @param {string} element - id of the element
+ * @param {string} selector - selector of the element
+ * @returns {undefined}
+ */
+
 function search() {
-   // TODO
+   let search = document.getElementById('searchText').value;
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let result = document.getElementById('result');
+   let count = 0;
+   towns.forEach((town) => {
+      if (town.textContent.includes(search)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         count++;
+      } else {
+         town.style.fontWeight = '';
+         town.style.textDecoration = '';
+      }
+   });
+   result.textContent = `${count} matches found`;
 }
