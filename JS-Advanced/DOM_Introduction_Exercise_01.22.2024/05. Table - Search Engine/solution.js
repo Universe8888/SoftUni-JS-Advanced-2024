cd5381ff6ac70505ec53e113607f5f9239498416
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 12:58:11 +0200

    Update solution.js

--- JS-Advanced/DOM_Introduction_Exercise_01.22.2024/05. Table - Search Engine/solution.js
+++ JS-Advanced/DOM_Introduction_Exercise_01.22.2024/05. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
+/**
+ * A function that searches for a given text inside a table and highlights the corresponding row, while hiding all other rows.
+ * 
+ * @param {HTMLElement} element - DOM element
+ * @param {string} selector - selector of the element
+ * @returns {undefined} - The function doesn’t return anything – it should modify the DOM directly.
+ * 
+ */
+
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      let search = document.getElementById('searchField').value;
+      let rows = Array.from(document.querySelectorAll('tbody tr'));
+      rows.forEach((row) => {
+         if (row.textContent.includes(search)) {
+            row.classList.add('select');
+         } else {
+            row.classList.remove('select');
+         }
+      });
+      document.getElementById('searchField').value = '';
    }
 }
