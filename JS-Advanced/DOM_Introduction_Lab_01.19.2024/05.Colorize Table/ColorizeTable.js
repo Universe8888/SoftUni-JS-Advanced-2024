ed720188d7fca966b65fe2f90604f7743c578d7e
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 11:22:33 +0200

    Update ColorizeTable.js

--- JS-Advanced/DOM_Introduction_Lab_01.19.2024/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/DOM_Introduction_Lab_01.19.2024/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
     // TODO
+
+    let rows = document.querySelectorAll('table tr');
+    let index = 0;
+    for (let row of rows) {
+        index++;
+        if (index % 2 == 0) {
+            row.style.backgroundColor = 'teal';
+        }
+    }
 }
