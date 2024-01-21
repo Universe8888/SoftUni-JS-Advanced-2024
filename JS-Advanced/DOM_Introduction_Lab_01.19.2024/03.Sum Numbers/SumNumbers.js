4dc74c2df2299b597a61683c28d88f4837fc1a5c
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 11:17:50 +0200

    Update SumNumbers.js

--- JS-Advanced/DOM_Introduction_Lab_01.19.2024/03.Sum Numbers/SumNumbers.js
+++ JS-Advanced/DOM_Introduction_Lab_01.19.2024/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
     // TODO: sum = num1 + num2
+
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    let sum = num1 + num2;
+
+    document.getElementById('sum').value = sum;
 }
