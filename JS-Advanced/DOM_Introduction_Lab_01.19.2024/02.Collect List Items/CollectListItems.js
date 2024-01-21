78b996d7cbf20f6f51a12d055464d57a928592e1
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 11:15:11 +0200

    Update CollectListItems.js

--- JS-Advanced/DOM_Introduction_Lab_01.19.2024/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/DOM_Introduction_Lab_01.19.2024/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.querySelectorAll('ul#items li');
+    let textArea = document.querySelector('#result');
+    
+    for (let item of items) {
+        textArea.value += item.textContent + '\n';
+    }
 }
