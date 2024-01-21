c062d096ba22ae6c9cddf1b16062a10479e62cd0
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-21 12:47:54 +0200

    Update solution.js

--- JS-Advanced/DOM_Introduction_Exercise_01.22.2024/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/DOM_Introduction_Exercise_01.22.2024/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
+/**
+ * Pascal or Camel Case
+ * @param {string} text
+ * @param {string} convention
+ * @returns {string}
+ */
+
 function solve() {
-  //TODO...
-}
+    let text = document.getElementById('text').value;
+    let convention = document.getElementById('naming-convention').value;
+    let result = document.getElementById('result');
+    let output = '';
+
+    if (convention === 'Camel Case') {
+        output = text.toLowerCase().split(' ').map((word, index) => {
+            if (index !== 0) {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            } else {
+                return word;
+            }
+        }).join('');
+    } else if (convention === 'Pascal Case') {
+        output = text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+    } else {
+        output = 'Error!';
+    }
+
+    result.textContent = output;
+  }
