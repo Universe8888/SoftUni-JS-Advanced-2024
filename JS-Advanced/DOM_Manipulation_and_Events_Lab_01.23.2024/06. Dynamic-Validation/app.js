f3c621191792b84f25ee971a3f0e8af94cf4f0d5
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-24 19:43:16 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/06. Dynamic-Validation/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.querySelector('#email');
+    input.addEventListener('change', onChange);
+
+    function onChange(ev) {
+        let regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        if (regex.test(ev.target.value)) {
+            ev.target.classList.remove('error');
+        } else {
+            ev.target.classList.add('error');
+        }
+    }
 }
