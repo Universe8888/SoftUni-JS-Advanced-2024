20c05d424471d43fa45f15714ff0ce2877168c1d
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-24 19:27:14 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/02. Delete-From-Table/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document.querySelector('input[name="email"]');
+
+    function deleteRow(row) {
+        row.parentNode.removeChild(row);
+    }
+
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    let deleted = false;
+    for (let row of rows) {
+        if (row.children[1].textContent == input.value) {
+            deleteRow(row);
+            deleted = true;
+        }
+    }
+    document.getElementById('result').textContent = deleted ? 'Deleted.' : 'Not found.';
+    input.value = '';
 }
