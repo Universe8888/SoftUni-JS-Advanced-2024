88a1efd549e4004d83409ae90ef1cf85afa581cb
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-24 19:54:05 +0200

    Update solution.js

--- JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/07. Shopping-Cart/solution.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/07. Shopping-Cart/solution.js
@@ -1,3 +1,41 @@
+/**
+ * The program must calculate the total price of the products that are selected.
+ * 
+ * @param {HTMLButtonElement[]} buttons
+ * @param {HTMLTextAreaElement} textArea
+ * @param {HTMLButtonElement} checkoutButton
+ * @param {string[]} products
+ * @param {number} totalPrice
+ * @param {Set<string>} uniqueProducts
+ * @param {string} name
+ * @param {number} price
+ * @returns {void}
+ * 
+ */
+
 function solve() {
-   //TODO...
+   let products = [];
+   let totalPrice = 0;
+   let textArea = document.querySelector('textarea');
+   let checkoutButton = document.querySelector('.checkout');
+   let addButtons = Array.from(document.querySelectorAll('.add-product'));
+   addButtons.forEach(btn => btn.addEventListener('click', addProduct));
+
+   function addProduct(e) {
+      let product = e.target.parentNode.parentNode;
+      let name = product.querySelector('.product-title').textContent;
+      let price = Number(product.querySelector('.product-line-price').textContent);
+      products.push(name);
+      totalPrice += price;
+      textArea.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+   }
+
+   checkoutButton.addEventListener('click', checkout);
+
+   function checkout() {
+      let uniqueProducts = new Set(products);
+      textArea.value += `You bought ${[...uniqueProducts].join(', ')} for ${totalPrice.toFixed(2)}.`;
+      addButtons.forEach(btn => btn.removeEventListener('click', addProduct));
+      checkoutButton.removeEventListener('click', checkout);
+   }
 }
