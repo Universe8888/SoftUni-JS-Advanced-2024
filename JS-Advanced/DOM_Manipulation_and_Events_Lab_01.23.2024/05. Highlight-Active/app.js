dd1fbdac2092771757b60ee7bac7d33d328e3417
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-24 19:34:43 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/05. Highlight-Active/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('input[type="text"]');
+
+    function focus(event) {
+        event.target.parentNode.className = 'focused';
+    }
+
+    function blur(event) {
+        event.target.parentNode.className = '';
+    }
+
+    function attachEvents() {
+        for (let input of inputs) {
+            input.addEventListener('focus', focus);
+            input.addEventListener('blur', blur);
+        }
+    }
+
+    attachEvents();
 }
