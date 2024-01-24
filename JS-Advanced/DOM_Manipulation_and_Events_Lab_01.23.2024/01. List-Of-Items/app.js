366d66cff80a72f1a1d582c474717c66b24f0a81
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-24 19:23:02 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/01. List-Of-Items/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Lab_01.23.2024/01. List-Of-Items/app.js
@@ -1,3 +1,8 @@
 function addItem() {
-    console.log('TODO:...');
+    let input = document.getElementById('newItemText');
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    let ul = document.getElementById('items');
+    ul.appendChild(li);
+    input.value = '';
 }
