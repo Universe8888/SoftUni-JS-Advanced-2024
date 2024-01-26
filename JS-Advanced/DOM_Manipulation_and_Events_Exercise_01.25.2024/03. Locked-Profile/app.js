e7528695bc4376bb5363f019cc40b63e451696dc
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 11:13:08 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/03. Locked-Profile/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/03. Locked-Profile/app.js
@@ -1,3 +1,28 @@
+/**
+ * @param {Event} e
+ * @returns {void}
+ */
+
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.getElementsByTagName('button');
+
+    Array.from(buttons).forEach(btn => { btn.addEventListener('click', showMore) });
+
+    function showMore(e) {
+        let profile = e.target.parentElement;
+        let isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            let div = profile.querySelector('div');
+            let isVisible = div.style.display === 'block';
+
+            if (isVisible) {
+                div.style.display = 'none';
+                e.target.textContent = 'Show more';
+            } else {
+                div.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            }
+        }
+    }
 }
