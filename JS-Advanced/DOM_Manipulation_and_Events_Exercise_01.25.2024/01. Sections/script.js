ca82d45ed0730058628b1712dd5ff79cf89d0dde
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 10:59:52 +0200

    Update script.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/01. Sections/script.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content');
+
+   for (let word of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', show);
+      content.appendChild(div);
+   }
+
+   function show(e) {
+      let p = e.target.children[0];
+      p.style.display = 'block';
+   }
 }
