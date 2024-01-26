e1ef7fb67c5b697c7293ae359a2859ef34b7dcb0
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 12:08:37 +0200

    Update solution.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/09. JavaScript-Quiz/solution.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,40 @@
+/**
+ * @param {HTMLElement} element
+ * @param {string[]} classNames
+ * @return {HTMLElement}
+ */
+
 function solve() {
-  //TODO...
+  let correctAnswers = 0;
+  let currentStep = 0;
+
+  const sections = document.querySelectorAll('section');
+
+  const correctAnswersObj = {
+      0: 'onclick',
+      1: 'JSON.stringify()',
+      2: 'A programming API for HTML and XML documents'
+  };
+
+  const handler = (e) => {
+      if (correctAnswersObj[currentStep] === e.target.textContent) {
+          correctAnswers++;
+      }
+
+      sections[currentStep].style.display = 'none';
+
+      if (sections[currentStep + 1] !== undefined) {
+          sections[currentStep + 1].style.display = 'block';
+      } else {
+          document.querySelector('#results').style.display = 'block';
+          document.querySelector('#results > li > h1').textContent = correctAnswers === 3
+              ? 'You are recognized as top JavaScript fan!'
+              : `You have ${correctAnswers} right answers`;
+      }
+
+      currentStep++;
+  };
+
+  [...document.querySelectorAll('.quiz-answer')]
+      .map((x) => x.addEventListener('click', handler));
 }
