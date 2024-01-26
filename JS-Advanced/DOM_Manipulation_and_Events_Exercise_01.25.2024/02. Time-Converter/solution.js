30d4699482fdedba654662fbffd9107de476aa91
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 11:08:28 +0200

    Update solution.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/02. Time-Converter/solution.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/02. Time-Converter/solution.js
@@ -1,4 +1,43 @@
-function attachEventsListeners() {
+/**
+ * @param {HTMLInputElement} days
+ * @param {HTMLInputElement} hours
+ * @param {HTMLInputElement} minutes
+ * @param {HTMLInputElement} seconds
+ * @returns {void}
+ */
 
-    console.log('TODO:...');
+function attachEventsListeners() {
+    const days = document.getElementById('days');
+    const hours = document.getElementById('hours');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+
+    const convertDays = () => {
+        hours.value = days.value * 24;
+        minutes.value = days.value * 1440;
+        seconds.value = days.value * 86400;
+    };
+
+    const convertHours = () => {
+        days.value = hours.value / 24;
+        minutes.value = hours.value * 60;
+        seconds.value = hours.value * 3600;
+    };
+
+    const convertMinutes = () => {
+        days.value = minutes.value / 1440;
+        hours.value = minutes.value / 60;
+        seconds.value = minutes.value * 60;
+    };
+
+    const convertSeconds = () => {
+        days.value = seconds.value / 86400;
+        hours.value = seconds.value / 3600;
+        minutes.value = seconds.value / 60;
+    };
+
+    document.getElementById('daysBtn').addEventListener('click', convertDays);
+    document.getElementById('hoursBtn').addEventListener('click', convertHours);
+    document.getElementById('minutesBtn').addEventListener('click', convertMinutes);
+    document.getElementById('secondsBtn').addEventListener('click', convertSeconds);
 }
