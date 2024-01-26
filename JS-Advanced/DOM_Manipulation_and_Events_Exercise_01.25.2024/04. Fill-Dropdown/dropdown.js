5ea1b7c6d9ad92ddb03ca9f7593799e017ffefb7
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 11:17:39 +0200

    Update dropdown.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
+/**
+ * @param {Event} event
+ * @returns {void}
+ */
+
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText').value;
+    let value = document.getElementById('newItemValue').value;
+
+    let option = document.createElement('option');
+    option.textContent = text;
+    option.value = value;
+
+    document.getElementById('menu').appendChild(option);
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
