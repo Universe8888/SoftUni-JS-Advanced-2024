57ac3c45f204dbb285ed706e716c7a38f81f04d4
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-26 11:21:15 +0200

    Update app.js

--- JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/DOM_Manipulation_and_Events_Exercise_01.25.2024/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeBtn = document.getElementsByTagName('button')[0];
+    let decodeBtn = document.getElementsByTagName('button')[1];
+    let encodeTextArea = document.getElementsByTagName('textarea')[0];
+    let decodeTextArea = document.getElementsByTagName('textarea')[1];
+
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+    function encode() {
+        let text = encodeTextArea.value;
+        let encodedText = '';
+        for (let i = 0; i < text.length; i++) {
+            encodedText += String.fromCharCode(text.charCodeAt(i) + 1);
+        }
+        encodeTextArea.value = '';
+        decodeTextArea.value = encodedText;
+    }
+
+    function decode() {
+        let text = decodeTextArea.value;
+        let decodedText = '';
+        for (let i = 0; i < text.length; i++) {
+            decodedText += String.fromCharCode(text.charCodeAt(i) - 1);
+        }
+        decodeTextArea.value = decodedText;
+    }
 }
