0f99485de0172d7aa44cabda738cb31bbe4ab9ac
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-27 19:33:24 +0200

    Update script.js

--- JS-Advanced/Advanced_Functions_Exercise_01.29.2024/08. Next Article/script.js
+++ JS-Advanced/Advanced_Functions_Exercise_01.29.2024/08. Next Article/script.js
@@ -1,3 +1,16 @@
+/**
+ * @param {string[]} articles
+ * @return {function}
+ */
+
 function getArticleGenerator(articles) {
-    // TODO
+    let content = document.getElementById('content');
+
+    return function () {
+        if (articles.length > 0) {
+            let article = document.createElement('article');
+            article.textContent = articles.shift();
+            content.appendChild(article);
+        }
+    }
 }
