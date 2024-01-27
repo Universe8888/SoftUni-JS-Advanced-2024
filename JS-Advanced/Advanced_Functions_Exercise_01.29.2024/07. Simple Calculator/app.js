d74f808944aa5dc725579757f1016ca77d1b3c8b
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-27 19:30:00 +0200

    Update app.js

--- JS-Advanced/Advanced_Functions_Exercise_01.29.2024/07. Simple Calculator/app.js
+++ JS-Advanced/Advanced_Functions_Exercise_01.29.2024/07. Simple Calculator/app.js
@@ -1,7 +1,32 @@
+/**
+ * @param {string} selector1
+ * @param {string} selector2
+ * @param {string} resultSelector
+ * @return {object}
+ */
+
 function calculator() {
-    // TODO:
+    let firstNumber;
+    let secondNumber;
+    let result;
+    
+    function init(selector1, selector2, resultSelector) {
+        firstNumber = document.querySelector(selector1);
+        secondNumber = document.querySelector(selector2);
+        result = document.querySelector(resultSelector);
+    }
+
+    function add() {
+        result.value = Number(firstNumber.value) + Number(secondNumber.value);
+    }
+
+    function subtract() {
+        result.value = Number(firstNumber.value) - Number(secondNumber.value);
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    }
 }
-
-
-
-
