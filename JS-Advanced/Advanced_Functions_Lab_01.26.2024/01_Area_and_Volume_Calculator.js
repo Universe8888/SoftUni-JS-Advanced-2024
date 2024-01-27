72eb5027592e6780e23c182fc855304a8378317f
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-27 10:44:44 +0200

    Create 01_Area_and_Volume_Calculator.js

---
+++ JS-Advanced/Advanced_Functions_Lab_01.26.2024/01_Area_and_Volume_Calculator.js
@@ -0,0 +1,43 @@
+function solve(area, vol, input) {
+    let objects = JSON.parse(input);
+    let result = [];
+
+    for (let obj of objects) {
+        let areaObj = Math.abs(area.call(obj));
+        let volumeObj = Math.abs(vol.call(obj));
+        result.push({area: areaObj, volume: volumeObj});
+    
+    }
+    return result;
+}
+
+
+
+function area() {
+
+    return Math.abs(this.x * this.y);
+    
+    };
+
+function vol() {
+
+    return Math.abs(this.x * this.y * this.z);
+        
+    };
+
+
+const input = [ area, vol, `[ {"x":"1","y":"2","z":"10"}, {"x":"7","y":"7","z":"10"}, {"x":"5","y":"2","z":"10"} ]` ];
+
+// const input = [area, vol, `[
+
+//     {"x":"10","y":"-22","z":"10"},
+    
+//     {"x":"47","y":"7","z":"-5"},
+    
+//     {"x":"55","y":"8","z":"0"},
+    
+//     {"x":"100","y":"100","z":"100"},
+    
+//     {"x":"55","y":"80","z":"250"}
+    
+//     ]`];
