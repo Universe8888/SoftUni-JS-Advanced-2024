17cf55fa60ba364fa3e72a0701076b385fe07b54
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-01-30 22:15:16 +0200

    Create 02_Playing_Cards.js

---
+++ JS-Advanced/Unit_Testing_and_Error_Handling_Lab_01.30.204/02_Playing_Cards.js
@@ -0,0 +1,33 @@
+function playingCards(face, suit) {
+    const validFaces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+    const validSuits = ['S','H','D','C'];
+
+    if (!validFaces.includes(face)) {
+        throw new Error('Invalid face!');
+    }
+
+    if (!validSuits.includes(suit)) {
+        throw new Error('Invalid suit!');
+    }
+
+    let suitToChar = {
+        'S': '\u2660',
+        'H': '\u2665',
+        'D': '\u2666',
+        'C': '\u2663'
+    }
+
+    return {
+        face,
+        suit,
+        toString() {
+            return `${face}${suitToChar[suit]}`;
+        }
+    }
+}
+
+console.log(playingCards('A', 'S').toString()); // A♠
+console.log('-----------------');
+console.log(playingCards('10', 'H').toString()); // 10♥
+console.log('-----------------');
+console.log(playingCards('1', 'C').toString()); // Error
