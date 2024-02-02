ccab0c976f6185bcbdfe35481f13db4e1406cbc6
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-02-02 10:19:24 +0200

    Update app.js

--- JS-Advanced/Unit_Testing_and_Error_Handling_Exercise_02.01.2024/06.Dynamic-Validation/app.js
+++ JS-Advanced/Unit_Testing_and_Error_Handling_Exercise_02.01.2024/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let email = document.getElementById('email');
+    email.addEventListener('change', onChange);
+
+    function onChange(ev) {
+        let email = ev.target.value;
+        let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        if (pattern.test(email)) {
+            ev.target.className = '';
+        } else {
+            ev.target.className = 'error';
+        }
+    }
 }
