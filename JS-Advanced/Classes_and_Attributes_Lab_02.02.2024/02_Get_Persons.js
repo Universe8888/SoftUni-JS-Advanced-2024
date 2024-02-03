7acee526d7400cf6cd3c3203e7a4205232523f5c
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-02-03 11:48:13 +0200

    Create 02_Get_Persons.js

---
+++ JS-Advanced/Classes_and_Attributes_Lab_02.02.2024/02_Get_Persons.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+
+    static getPersons() {
+        return [
+            new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+            new Person('SoftUni'),
+            new Person('Stephan', 'Johnson', 25),
+            new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+        ];
+    }
+}
