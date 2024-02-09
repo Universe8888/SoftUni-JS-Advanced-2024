dac7983d5c6256798a4a841bb5a5fc08e0e5287a
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-02-09 18:39:50 +0200

    Create 03_Extensible_Object.js

---
+++ JS-Advanced/Prototypes_and_Inheritance_Exercise_02.08.2024/03_Extensible_Object.js
@@ -0,0 +1,14 @@
+function extensibleObject() {
+    let obj = {
+        extend: function (template) {
+            for (let prop in template) {
+                if (typeof template[prop] === 'function') {
+                    Object.getPrototypeOf(obj)[prop] = template[prop];
+                } else {
+                    obj[prop] = template[prop];
+                }
+            }
+        }
+    };
+    return obj;
+}
