811db91efd3a259be63387ed3ce94125d7ded1d3
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-02-29 21:56:48 +0200

    Update app.js

--- JS-Applications/Exercise_Asynchronous_Programming_02.26.2024/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS-Applications/Exercise_Asynchronous_Programming_02.26.2024/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,35 @@
+/**
+ * You are assigned to create a simple HTML page that shows information about buses.
+ * You will receive a JSON object that contains an array of buses. Each bus has a name and a time (number) when it will arrive at the bus stop.
+ * The information has to be shown in the following format:
+ *    "Bus {bus name} arrives in {time} minutes"
+ * The name and the time of the bus should be shown in an unordered list.
+ * The stop name is a heading 3 element.
+ * The buses should be shown when the "Show Bus" button is clicked.
+ * The stop name and the buses should be cleared when the "Clear" button is clicked.
+ */
+
 function getInfo() {
-    console.log("TODO...");
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const buses = document.getElementById('buses');
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            stopName.textContent = data.name;
+            buses.innerHTML = '';
+            Object.entries(data.buses).forEach(([bus, time]) => {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${bus} arrives in ${time} minutes`;
+                buses.appendChild(li);
+            });
+        })
+        .catch(() => {
+            stopName.textContent = 'Error';
+            buses.innerHTML = '';
+        });
 }
+
+//100/100
