743bfd05a8497c99eada74628d94445275b3cd50
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-03-02 23:18:21 +0200

    Update accordion.js

--- JS-Applications/Exercise_Asynchronous_Programming_02.26.2024/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
+++ JS-Applications/Exercise_Asynchronous_Programming_02.26.2024/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
@@ -1,3 +1,41 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const main = document.getElementById('main');
+
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list');
+    const data = await response.json();
+
+    data.forEach(article => {
+        const divAccordion = document.createElement('div');
+        divAccordion.className = 'accordion';
+
+        divAccordion.innerHTML = `
+            <div class="head">
+                <span>${article.title}</span>
+                <button class="button" id="${article._id}">More</button>
+            </div>
+            <div class="extra">
+                <p></p>
+            </div>
+        `;
+
+        main.appendChild(divAccordion);
+
+        const button = divAccordion.querySelector('.button');
+        button.addEventListener('click', async () => {
+            const extraDiv = button.parentElement.nextElementSibling;
+            if (button.textContent === 'More') {
+                const detailsResponse = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${article._id}`);
+                const detailsData = await detailsResponse.json();
+
+                extraDiv.style.display = 'block';
+                extraDiv.querySelector('p').textContent = detailsData.content;
+                button.textContent = 'Less';
+            } else {
+                extraDiv.style.display = 'none';
+                button.textContent = 'More';
+            }
+        });
+    });
 }
+
+solution();
