076491c53824e669d17b8ad7d98595e829943c9e
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-03-04 14:40:58 +0200

    Update app.js

--- JS-Applications/Applications_Data_and_Authentication_Exercise_02.29.2024/01.Messenger/app.js
+++ JS-Applications/Applications_Data_and_Authentication_Exercise_02.29.2024/01.Messenger/app.js
@@ -1,5 +1,31 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', sendMessage);
+    document.getElementById('refresh').addEventListener('click', loadMessages);
+}
+
+async function sendMessage() {
+    const author = document.querySelector('input[name="author"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+
+    const response = await fetch('/jsonstore/messenger', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ author, content })
+    });
+
+    if (response.ok) {
+        document.querySelector('input[name="author"]').value = '';
+        document.querySelector('input[name="content"]').value = '';
+    }
+}
+
+async function loadMessages() {
+    const response = await fetch('/jsonstore/mesensger');
+    const data = await response.json();
+    const messages = Object.values(data).map(({ author, content }) => `${author}: ${content}`).join('\n');
+    document.getElementById('messages').value = messages;
 }
 
 attachEvents();
