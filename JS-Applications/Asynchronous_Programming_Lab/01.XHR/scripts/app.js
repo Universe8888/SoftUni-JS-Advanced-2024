3c3ee52f2d40bc37ae64ba15d3704d8c0b26ba06
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-04-07 21:03:07 +0300

    Update app.js

--- JS-Applications/Asynchronous_Programming_Lab/01.XHR/scripts/app.js
+++ JS-Applications/Asynchronous_Programming_Lab/01.XHR/scripts/app.js
@@ -1,3 +1,10 @@
 function loadRepos() {
-   console.log("TODO...");
+   let request = new XMLHttpRequest();
+   request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+         document.getElementById('res').textContent = this.responseText;
+      }
+   };
+   request.open('GET', 'https://api.github.com/users/testnakov/repos', true);
+   request.send();
 }
