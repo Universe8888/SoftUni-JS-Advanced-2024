8bcf8232e6a3f8a1809947fbef13e41adceec7a8
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-04-13 21:20:09 +0300

    Update app.js

--- JS-Applications/Asynchronous_Programming_Lab/03.Gighub-Commits/app.js
+++ JS-Applications/Asynchronous_Programming_Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,33 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let ul = document.getElementById('commits');
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(response => {
+            if (response.status >= 400) {
+                throw new Error('Invalid request');
+            }
+            return response.json();
+        }
+        )
+
+        .then(data => {
+            ul.innerHTML = '';
+            data.forEach(element => {
+                let li = document.createElement('li');
+                li.textContent = `${element.commit.author.name}: ${element.commit.message}`;
+                ul.appendChild(li);
+            });
+        })
+
+        .catch(err => {
+            let li = document.createElement('li');
+            li.textContent = `Error: ${err.message} (Not Found)`;
+            ul.appendChild(li);
+        });
+
+    document.getElementById('username').value = '';
+    document.getElementById('repo').value = '';
 }
