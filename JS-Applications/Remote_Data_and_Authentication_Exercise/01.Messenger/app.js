243de6199fa58e19b3cc9c4c717931d6bda86d30
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2024-05-06 12:42:57 +0300

    Update app.js

--- JS-Applications/Remote_Data_and_Authentication_Exercise/01.Messenger/app.js
+++ JS-Applications/Remote_Data_and_Authentication_Exercise/01.Messenger/app.js
@@ -1,5 +1,49 @@
 function attachEvents() {
-    console.log('TODO...');
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+    const messages = document.getElementById('messages');
+    const author = document.querySelector('input[name="author"]');
+    const content = document.querySelector('input[name="content"]');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    submitBtn.addEventListener('click', sendMessage);
+    refreshBtn.addEventListener('click', refreshMessages);
+
+    function sendMessage() {
+        if (author.value === '' || content.value === '') {
+            return;
+        }
+
+        const message = { author: author.value, content: content.value };
+
+        fetch(baseUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(message)
+        })
+            .then(response => response.json())
+            .then(() => {
+                author.value = '';
+                content.value = '';
+                refreshMessages();
+            });
+    }
+
+    function refreshMessages() {
+        messages.innerHTML = '';
+
+        fetch(baseUrl)
+            .then(response => response.json())
+            .then(data => {
+                Object.values(data).forEach(message => {
+                    const { author, content } = message;
+                    const messageText = `${author}: ${content}`;
+                    messages.innerHTML += messageText + '\n';
+                });
+            });
+    }
+
+    refreshMessages();
 }
 
 attachEvents();
